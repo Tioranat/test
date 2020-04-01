2ec8a599e16b7cba318d14c974693e73b2c0ca57
Tioranat.info <62396828+Tioranat@users.noreply.github.com>
2020-04-01 12:37:27 +0200

    new structure json

--- index.js
+++ index.js
@@ -1,75 +1,58 @@
-var DB;
+
+/* jshint undef: true, unused: true */
+/* globals $ */
+/*jshint strict:false */
+/* jshint -W097 */
+
+"use strict";
+
 $(function () {
     loadHashReplace();
     loadDB(function (Data) {
-        DB = Data;
-        //loadSymptoms();
-        loadRadio();
-        loadCheckbox();
+        loadTable(Data,"nav-infection","InfectionRisk");
+        loadTable(Data,"nav-group","RiskGroup");
     });
 });
 
 
-function loadCheckbox(){
-    var NewTab = new table();
-    NewTab.loadHeader(['Group', 'Question','weight']);
 
-    var Survey;
-    var Abswers;
-    var k;
-    for (var k = 0; k < DB["survey"].length; k++) {
-        if (DB["survey"][k]["type"] == "radiocheck") {
-            Survey = DB["survey"][k];
-            NewTab.addRow([Survey["group"],Survey["name"],Survey["weight"]])
-        }
-    }
-    NewTab.putHTML("#nav-checkbox");
+function loadTable(DB,ID,Group) {
+    var NewTab = new Table();
+    NewTab.loadHeader(['Question','Answer', 'Weight']);
 
-}
-
-function loadRadio() {
-    var NewTab = new table();
-    NewTab.loadHeader(['Group', 'Question','Answer', 'Weight']);
-
-    var Survey;
-    var Abswers;
+    var QuestionArr = Object.keys(DB.questions);
+    var Question;
+    var QuestionSet ;
+    var AnswerArr;
+    var Answer;
     var k;
     var l;
-    for (k = 0; k < DB["survey"].length; k++) {
-        if (DB["survey"][k]["type"] == "radio") {
-            Survey = DB["survey"][k];
-            Answers = Survey["answers"];
-            for (l = 0; l < Answers.length; l++) {
-                NewTab.addRow([Survey["group"],Survey["name"], Answers[l]["answer"], Answers[l]["weight"]])
+
+    for (k = 0; k < QuestionArr.length; k++) {
+        Question = QuestionArr[k];
+        QuestionSet = DB.questions[Question];
+        if (QuestionSet.group === Group) {
+            AnswerArr = Object.keys(QuestionSet.answers);
+            for(l=0;l < AnswerArr.length;l++){
+                Answer = AnswerArr[l];
+                NewTab.addRow([Question, Answer, QuestionSet.answers[Answer]]);
             }
         }
     }
-    NewTab.putHTML("#nav-radio");
-
-}
-
-function loadSymptoms() {
-    var NewTab = new table();
-    NewTab.loadHeader(['Key', 'Weight', 'Intensity [0-5]']);
-
-    var Survey;
-    for (var k = 0; k < DB["survey"].length; k++) {
-        if (DB["survey"][k]["name"] == "QuestionSymptoms") {
-            Survey = DB["survey"][k]["answers"];
-        }
-    }
-    for (var k = 0; k < Survey.length; k++) {
-        NewTab.addRow([Survey[k]["answer"], Survey[k]["weight"], Survey[k]["target"]]);
-    }
-    NewTab.putHTML("#nav-sym");
+    NewTab.putHTML("#"+ID);
 }
 
 
-function table() {
+
+/**
+ * Class table
+ * @constructor
+ */
+function Table() {
     this.Rows = [];
     this.Headers = [];
     this.putHTML = function (ID) {
-        var HTML = '<table class="table table-border"><tr>';
+        var HTML = '<table class="table table-bordered"><tr>';
         var k;
         var l;
         for (k = 0; k < this.Headers.length; k++) {
@@ -85,22 +68,28 @@ function table() {
         }
         HTML += "</table>";
         $(ID).html(HTML);
-    }
+    };
 
     this.loadHeader = function (Headers) {
         this.Headers = Headers;
-    }
+    };
 
     this.addRow = function (Row) {
         this.Rows.push(Row);
-    }
+    };
 }
 
 function loadDB(nextFunction) {
+    var URL;
+    if(location.hostname === "tioranat.github.io"){
+        URL = "https://raw.githubusercontent.com/Tioranat/tioranat.info/master/rki/survey.json";
+    }else{
+        URL = "/survey.json";
+    }
     $.ajax({
         'async': false,
         'global': false,
-        'url': "https://raw.githubusercontent.com/Tioranat/tioranat.info/master/rki/survey.json",
+        'url': URL,
         'dataType': "json",
         'success': function (data) {
             nextFunction(data);
@@ -109,15 +98,13 @@ function loadDB(nextFunction) {
 }
 
 function loadHashReplace() {
-    if (location.hash.substr(0, 2) == "#!") {
+    if (location.hash.substr(0, 2) === "#!") {
         $("a[href='#" + location.hash.substr(2) + "']").tab("show");
-    }
-    ;
+    };
     $("a[data-toggle='tab']").on("shown.bs.tab", function (e) {
         var hash = $(e.target).attr("href");
-        if (hash.substr(0, 1) == "#") {
+        if (hash.substr(0, 1) === "#") {
             location.replace("#!" + hash.substr(1));
-        }
-        ;
+        };
     });
 }
